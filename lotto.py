1d1f7ff2c14ab061bd1fe7816d15b0008f82b9a2
fracasti07 <116189156+fracasti07@users.noreply.github.com>
2023-02-27 15:36:17 +0100

    Add files via upload

---
+++ lotto.py
@@ -0,0 +1,15 @@
+import random
+numero_intero_casuale = random.randint(1,90)
+print(numero_intero_casuale)
+import random
+numero_intero_casuale = random.randint(1,90)
+print(numero_intero_casuale)
+import random
+numero_intero_casuale = random.randint(1,90)
+print(numero_intero_casuale)
+import random
+numero_intero_casuale = random.randint(1,90)
+print(numero_intero_casuale)
+import random
+numero_intero_casuale = random.randint(1,90)
+print(numero_intero_casuale)
